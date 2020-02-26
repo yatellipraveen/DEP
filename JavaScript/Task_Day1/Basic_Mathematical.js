cf3f22f9becfa10a2b4a27e99f187bf8332b7002
Praveen Yatelli <Praveen_Yatelli@epam.com>
2020-02-26 13:46:15 +0530

    Javascript Task1

---
+++ JavaScript/Task_Day1/Basic_Mathematical.js
@@ -0,0 +1,8 @@
+https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+//Mathematical_Operations
+
+
+function basicOp(operation, value1, value2)
+{ 
+ return eval(value1+operation+value2);
+}
---
+++ JavaScript/Task_Day1/Transportation_on_Vacation.js
@@ -0,0 +1,12 @@
+//https://www.codaysewars.com/kata/568d0dd208ee69389d000016/train/javascript
+
+
+// Transportation
+
+
+function rentalCarCost(days) {
+    var total=40*days;
+    if(days>=7)  total=total-50;
+    else if(days>=3) total=total-20;
+    return total;
+  }
