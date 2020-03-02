d31b725f9867185d417d3e5c910425346cf724ee
Praveen Yatelli <Praveen_Yatelli@epam.com>
2020-03-02 17:30:48 +0530

    If else changed to switch case

--- JavaScript/Task_Day1/Basic_Mathematical.js
+++ JavaScript/Task_Day1/Basic_Mathematical.js
@@ -2,13 +2,21 @@
 //Mathematical_Operations
 
 function basicOp(operation, value1, value2) {
-  if (operation == "+") {
-    return value1 + value2;
-  } else if (operation == "-") {
-    return value1 - value2;
-  } else if (operation == "*") {
-    return value1 * value2;
-  } else if (operation == "/") {
-    return value1 / value2;
+  switch (operation) {
+    case "+": {
+      return value1 + value2;
+    }
+    case "-": {
+      return value1 - value2;
+    }
+    case "*": {
+      return value1 * value2;
+    }
+    case "/": {
+      return value1 / value2;
+    }
+    default: {
+      console.log("Invalid Operation");
+    }
   }
 }
