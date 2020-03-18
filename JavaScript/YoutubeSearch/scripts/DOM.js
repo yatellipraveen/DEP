8be2b0f9562f5e48414ffff4129a01b41f3b4e8e
Praveen Yatelli <Praveen_Yatelli@epam.com>
2020-03-18 10:31:27 +0530

    moved DOM elements to functions and calling on init function

--- JavaScript/YoutubeSearch/scripts/DOM.js
+++ JavaScript/YoutubeSearch/scripts/DOM.js
@@ -1,53 +1,103 @@
 //global variable declaration
+
+(function init() {
+  headerDiv();
+  searchDiv();
+  displayDiv();
+  navigationDiv();
+})();
+
 //creating header
-const header = document.createElement("header");
+function headerDiv() {
+  const header = document.createElement("header");
+  //creating h1 tag
+  const title = document.createElement("h1");
+  const titleText = document.createTextNode("Youtube Search");
+  title.appendChild(titleText);
+  header.appendChild(title);
 
-//creating h1 tag
-const title = document.createElement("h1");
-const titleText = document.createTextNode("Youtube Search");
-title.appendChild(titleText);
-header.appendChild(title);
+  const body = document.querySelector("body");
+  body.appendChild(header);
+}
 
-//input tag
-const searchBox = document.createElement("input");
-searchBox.setAttribute("type", "text");
-searchBox.setAttribute("id", "searchbox");
-searchBox.setAttribute("placeholder", "search");
+function searchDiv() {
+  //input tag
+  const searchBox = document.createElement("input");
+  searchBox.setAttribute("type", "text");
+  searchBox.setAttribute("id", "searchbox");
+  searchBox.setAttribute("placeholder", "search");
 
-const searchButton = document.createElement("button");
-searchButton.innerHTML = "&#128269;";
+  const searchButton = document.createElement("button");
+  searchButton.innerHTML = "&#128269;";
 
-//search div
-const searchDiv = document.createElement("div");
-searchDiv.classList.add("search");
-searchDiv.appendChild(searchBox);
-searchDiv.appendChild(searchButton);
+  //search div
+  const searchDiv = document.createElement("div");
+  searchDiv.classList.add("search");
+  searchDiv.appendChild(searchBox);
+  searchDiv.appendChild(searchButton);
+
+  const body = document.querySelector("body");
+  body.appendChild(searchDiv);
+
+  //event listeners
+  searchButton.addEventListener("click", async event => {
+    query = searchBox.value;
+    await getVideos(url + query);
+    pageIndex++;
+    populateView(pageIndex);
+    nextButton.style.display = "block";
+  });
+}
 
 //display div
-const displayDiv = document.createElement("div");
-displayDiv.classList.add("display");
+function displayDiv() {
+  const displayDiv = document.createElement("div");
+  displayDiv.classList.add("display");
 
-//previous button
-const previousButton = document.createElement("button");
-previousButton.classList.add("previous");
-previousButton.textContent = "Previous";
+  const body = document.querySelector("body");
+  body.appendChild(displayDiv);
+}
 
-//next Button
-const nextButton = document.createElement("button");
-nextButton.classList.add("next");
-nextButton.textContent = "Next";
+function navigationDiv() {
+  //previous button
+  const previousButton = document.createElement("button");
+  previousButton.classList.add("previous");
+  previousButton.textContent = "Previous";
 
-//navigation div
-const navigationDiv = document.createElement("div");
-navigationDiv.classList.add("navigation");
-navigationDiv.appendChild(previousButton);
-navigationDiv.appendChild(nextButton);
+  //next Button
+  const nextButton = document.createElement("button");
+  nextButton.classList.add("next");
+  nextButton.textContent = "Next";
 
-const body = document.querySelector("body");
-body.appendChild(header);
-body.appendChild(searchDiv);
-body.appendChild(displayDiv);
-body.appendChild(navigationDiv);
+  //navigation div
+  const navigationDiv = document.createElement("div");
+  navigationDiv.classList.add("navigation");
+  navigationDiv.appendChild(previousButton);
+  navigationDiv.appendChild(nextButton);
+
+  const body = document.querySelector("body");
+  body.appendChild(navigationDiv);
+
+  previousButton.addEventListener("click", event => {
+    clearView();
+    pageIndex--;
+    populateView(pageIndex);
+  });
+
+  nextButton.addEventListener("click", event => {
+    clearView();
+    pageIndex++;
+    populateView(pageIndex);
+  });
+
+  //clear the view on the page
+  function clearView() {
+    const cardOneImg = document.querySelectorAll(".card");
+    for (let i = 0; i < cardOneImg.length; i++) {
+      displayDiv.removeChild(cardOneImg[i]);
+    }
+  }
+}
 
 let pageIndex = -1;
 let items = new Array();
@@ -58,27 +108,6 @@ const url =
 let query = "";
 let nextPageToken = "";
 let previousPageToken = "";
-
-//event listeners
-searchButton.addEventListener("click", async event => {
-  query = searchBox.value;
-  await getVideos(url + query);
-  pageIndex++;
-  populateView(pageIndex);
-  nextButton.style.display = "block";
-});
-
-previousButton.addEventListener("click", event => {
-  clearView();
-  pageIndex--;
-  populateView(pageIndex);
-});
-
-nextButton.addEventListener("click", event => {
-  clearView();
-  pageIndex++;
-  populateView(pageIndex);
-});
 
 //function to fetch data and add to the global list items
 function getVideos(queryUrl) {
@@ -95,15 +124,8 @@ function getVideos(queryUrl) {
     });
 }
 
-//clear the view on the page
-function clearView() {
-  const cardOneImg = document.querySelectorAll(".card");
-  for (let i = 0; i < cardOneImg.length; i++) {
-    displayDiv.removeChild(cardOneImg[i]);
-  }
-}
-
 async function populateView(pageIndex) {
+  let previousButton = document.querySelector(".previous");
   if (pageIndex <= 0) {
     previousButton.style.display = "none";
   } else previousButton.style.display = "block";
@@ -137,6 +159,8 @@ async function populateView(pageIndex) {
     card.appendChild(img);
     card.appendChild(title);
     card.appendChild(description);
+
+    let displayDiv = document.querySelector(".display");
     displayDiv.appendChild(card);
   }
 }
