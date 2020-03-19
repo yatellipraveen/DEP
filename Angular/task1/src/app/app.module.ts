cf1fcc1cb0569f1b6285f20a07ac7690efe30673
Praveen Yatelli <Praveen_Yatelli@epam.com>
2020-03-19 15:50:56 +0530

    Angular_Task1

---
+++ Angular/task1/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ManualComponent } from './manual/manual.component';
+import { CliComponent } from './cli/cli.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ManualComponent,
+    CliComponent
+  ],
+  imports: [
+    BrowserModule,
+  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
